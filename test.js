8f2bfa55f262963bf245c9263df0269a4e5c76bd
joe <ruizjoseph17@gmail.com>
2019-09-02 14:55:10 -0400

    9/2/2019
    Update

--- test.js
+++ test.js
@@ -1,11 +1,26 @@
-eagerness = () => {
-    var eagernessList = ["Cautious, but can be convinced to hire",
-        "Willing to promise standard rates",
-        "Eager, willing to offer a bonus",
-        "Desperate, might offer what they can’t pay"];
+backGround = () => {
+    var backGroundList = [ 
+    ' The local under class or poorest natives ',
+    ' Common laborers or cube workers ', 
+    ' Aspiring bourgeoise or upper class ', 
+    ' The elite of this society ', 
+    ' Minority or foreigners; ', //roll 1d4
+    ' Offworlders or exotics; ']; //roll 1d4
+
+    rollDice = (numberOfDice) => {
+        return Math.floor(Math.random() * numberOfDice);
+    }
+
+    let k = rollDice(6);
+    output = backGroundList[k];
     
-        let k = Math.floor(Math.random() *4);
-        output = eagernessList[k];
-        document.getElementById("eagerness").innerHTML = "Patron Eagerness to Hire: " + output;
+    if (output == (backGroundList[4] || backGroundList[5])) {
+        let v = rollDice(4);
+        newOutput = backGroundList[v];
+        document.getElementById("backGround").innerHTML = "Background: " + output + newOutput;
+    }
+    else {
+        document.getElementById("backGround").innerHTML = "Background: " + output;
+    }
 }
-eagerness();
+backGround();
