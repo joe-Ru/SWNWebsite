18d2246cf3b1f99e7d9fd68a622c35d01effa46c
sigma0wl <ruizjoseph17@gmail.com>
2019-12-09 19:35:38 -0500

    Combat.js, added drop down menu

--- CombatNPC.js
+++ CombatNPC.js
@@ -44,7 +44,7 @@ userInput = () => {
         weaponRolled = () => {
             let k = Math.floor(Math.random() *3);
             weaponsList = [
-                "Mag Pistol: 2d6 + 2", "Spike Thrower: 3d8", "Rifle: 1d10 + 2"
+                "Laser Pistol: 1d6", "Semi Auto-Pistol: 1d6 + 1", "Submachine Gun: 1d8"
             ];
             return weaponsList[k];
         }
@@ -66,7 +66,7 @@ userInput = () => {
         weaponRolled = () => {
             let k = Math.floor(Math.random() *3);
             weaponsList = [
-                ""
+                "Mag Pistol: 2d6 + 2", "Spike Thrower: 3d8", "Rifle: 1d10 + 2"
             ];
             return weaponsList[k];
         }
@@ -79,7 +79,7 @@ userInput = () => {
         posthitDie.innerHTML = "Hit Points: " + hitDie;
 
         var postAtkDamage = document.getElementById("Atk");
-        postAtkDamage.innerHTML = "Atk bonus: +1";
+        postAtkDamage.innerHTML = "Atk bonus: +6";
 
         var postArmorClass = document.getElementById("Armor Class");
         postArmorClass.innerHTML = "Armor Class: 20";
@@ -88,7 +88,7 @@ userInput = () => {
         weaponRolled = () => {
             let k = Math.floor(Math.random() *3);
             weaponsList = [
-                "Laser Pistol: 1d6", "Semi Auto-Pistol: 1d6 + 1", "Submachine Gun: 1d8"
+                "Modified Mag Pistol: 3d6 + 2", "Spike Thrower: 3d8", "Combat Rifle: 1d12"
             ];
             return weaponsList[k];
         }
