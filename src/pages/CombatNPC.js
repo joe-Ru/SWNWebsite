5362b54b3fd00f36da14fe280d18c88ed8d23f4b
joseph <ruizjoseph17@gmail.com>
2020-06-06 22:52:58 -0400

    Testing change.

--- src/pages/CombatNPC.js
+++ src/pages/CombatNPC.js
@@ -2,12 +2,44 @@ import React from "react"
 import Layout from "../components/layout"
 
 
+
+
+
+
 class CombatNPC extends React.Component {   
    
     render() {
        return <div>
          <Layout>
-        <div align="center"><h1> In Progress</h1></div>
+           <div className="container">
+             <div className="row">
+               <div className="col-sm-12">
+
+                 <form onSubmit={this.handleFormSubmit}>
+                   <div className="radio">
+                     <label>
+                       <input type="radio" value="option1" checked={this.state.selectedOption === 'option1'} onChange={this.handleOptionChange} />
+                       Option 1
+                     </label>
+                   </div>
+                   <div className="radio">
+                     <label>
+                       <input type="radio" value="option2" checked={this.state.selectedOption === 'option2'} onChange={this.handleOptionChange}/>
+                       Option 2
+                     </label>
+                   </div>
+                   <div className="radio">
+                     <label>
+                       <input type="radio" value="option3" checked={this.state.selectedOption === 'option3'} onChange={this.handleOptionChange}/>
+                       Option 3
+                     </label>
+                   </div>
+                   <button className="btn btn-default" type="submit">Save</button>
+                 </form>
+
+               </div>
+             </div>
+           </div>
          </Layout>
        </div>
     }
